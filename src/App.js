4e3d832830e97d87835cca7bac1233791b79cbb5
Steven Kussmaul <sjkussmaul@gmail.com>
2019-04-14 16:57:16 -0400

    continuing build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 // import './App.css';
+import Todos from './components/Todos.js';
 
 class App extends Component {
+  state = {
+    todos: 
+    [
+      {
+        id: '1',
+        title: 'trash',
+        completed: 'false'
+      },
+      {
+        id: '2',
+        title: 'Dinner',
+        completed: 'false'
+      },
+      {
+        id: '3',
+        title: 'walk Dog',
+        completed: 'false'
+      }
+
+    ]
+  }
+
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
         <h1>hello world</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+
+class Todos extends Component {
+  render() {
+      console.log(this.props.todos)
+    return (
+        this.props.todos.map((todo)=> (
+            <TodoItem todo={todo}/>
+        ))
+    );
+  }
+}
+
+export default Todos;
