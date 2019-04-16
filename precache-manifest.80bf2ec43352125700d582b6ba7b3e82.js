613ac2d37fedf347b639a641b32bd29600fc8d0d
Steven Kussmaul <sjkussmaul@gmail.com>
2019-04-15 20:19:29 -0400

    Updates

---
+++ precache-manifest.80bf2ec43352125700d582b6ba7b3e82.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed63381bb10a3952e6a",
+    "url": "/todo-app-react/static/js/runtime~main.ec7f1ccf.js"
+  },
+  {
+    "revision": "d80bfe4b8367de12d2dd",
+    "url": "/todo-app-react/static/js/main.a248733d.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/todo-app-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ce8f40ae6157bfefc60b04a80a7368a4",
+    "url": "/todo-app-react/index.html"
+  }
+];
