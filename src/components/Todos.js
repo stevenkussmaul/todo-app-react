c8d8d1f688d886453338fd525256ee43a41ccb08
Steven Kussmaul <sjkussmaul@gmail.com>
2019-05-20 16:27:44 -0400

    added unique key to todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,8 +6,8 @@ class Todos extends Component {
   render() {
       console.log(this.props.todos)
     return (
-        this.props.todos.map((todo)=> (
-            <TodoItem todo={todo}/>
+        this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo}/>
         ))
     );
   }
